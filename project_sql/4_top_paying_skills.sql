eb2c9e4ae9c0b6c096f8cce2edb41a52207803a3
Anna-Roman <148797590+Anna-Roman@users.noreply.github.com>
2024-03-26 11:06:08 +0200

    commit message

--- project_sql/4_top_paying_skills.sql
+++ project_sql/4_top_paying_skills.sql
@@ -19,34 +19,39 @@ ORDER BY
     avg_salary DESC
 LIMIT 25;
 
-/*Breakdown of the results.
+SELECT 
+    skills,
+    ROUND(AVG(salary_year_avg),2) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Engineer' AND
+    salary_year_avg IS NOT NULL
+GROUP BY 
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25;
+
+/*Breakdown of the results for Data Analyst roles.
 Top paying skills are used in areas like:
-Software Development and Engineering: 
-Skills like Golang, Solidity, and GitLab are commonly used in 
-software development and engineering roles, particularly in 
-blockchain development, version control, and collaboration.
+Software Development and Engineering.
+Data Analysis and Machine Learning. 
+Infrastructure Management and DevOps.
+Data Storage and Processing.
+Workflow Automation and Collaboration.
+Version Control and Documentation.
 
-Data Analysis and Machine Learning: 
-Skills like TensorFlow, PyTorch, Keras, and MXNet are essential for 
-data analysts and machine learning engineers working on building and 
-training machine learning models for data analysis and prediction tasks.
+Breakdown of the results for Data Engineer roles.
+Top paying skills are used in areas like:
+Backend Development.
+Data Visualization and Analysis.
+Cloud Computing and Infrastructure.
+Big Data Technologies.
+Machine Learning and AI.
+Security and Compliance.
+Development and Operations (DevOps).
+Frontend Development.
+*/
 
-Infrastructure Management and DevOps: 
-Skills like Terraform, Puppet, Ansible, and VMware are crucial for 
-managing data infrastructure, automating deployment processes, 
-and ensuring efficient operations, often in DevOps roles.
-
-Data Storage and Processing: 
-Skills like Cassandra, Couchbase, and Apache Kafka are utilized in
-managing large-scale data storage, processing, and streaming applications, 
-often in data engineering roles.
-
-Workflow Automation and Collaboration: 
-Tools like Apache Airflow, Atlassian (Jira, Confluence), 
-and Notion are used for workflow automation, project management, 
-and collaboration among data teams and across departments.
-
-Version Control and Documentation: 
-Skills like SVN, GitLab, and Bitbucket are essential for version control 
-and collaboration on code and data projects, ensuring proper documentation 
-and tracking of changes.*/
